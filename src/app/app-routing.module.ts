e9ef317b94379f5e654d23c731c1ee69cb559e76
Joseph Ng <hsieangng@logitech.com>
2020-08-10 11:09:22 +0800

    Refactor create a new list css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
-import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewListComponent } from './pages/new/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new/new-task/new-task.component';
 
 const routes: Routes = [
   { path: '', component: TaskViewComponent },
   { path: 'new-list', component: NewListComponent },
+  { path: 'new-task', component: NewTaskComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
-import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewListComponent } from './pages/new/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new/new-task/new-task.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskViewComponent, NewListComponent],
+  declarations: [
+    AppComponent,
+    TaskViewComponent,
+    NewListComponent,
+    NewTaskComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
