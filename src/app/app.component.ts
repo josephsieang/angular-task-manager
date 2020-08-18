cdfb11aa22b32c2ef8368806ef142a6b3b69fd49
Joseph Ng <hsieangng@logitech.com>
2020-08-18 09:36:40 +0800

    Configure the AuthConfig in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'task-manager';
+
+  constructor(private authenticationService: AuthenticationService) {
+    this.authenticationService.configureOAuth();
+  }
 }
